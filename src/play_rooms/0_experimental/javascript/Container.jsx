2310ccdcb6d7873a4d12c2cc7821185735b8db3b
sophusma <masophus@gmail.com>
2025-04-16 00:27:55 +0300

    я запуталась

--- src/play_rooms/0_experimental/javascript/Container.jsx
+++ src/play_rooms/0_experimental/javascript/Container.jsx
@@ -18,6 +18,7 @@ export default class Container extends Component {
 
     this.state = {
       isStarted: false,
+      selectedSynth: null,
 
       activePresets: {
         melody: false,
@@ -27,7 +28,6 @@ export default class Container extends Component {
       bassSoundPreset: 'default',
       melodySoundPreset: 'default',
       padSoundPreset: 'default',
-      bpm: 80,
 
       //БАСС
       bassVolume: -25,
@@ -38,7 +38,12 @@ export default class Container extends Component {
       melodyVolume: -20,
 
       //ПЭД
-      padVolume: -12
+      padVolume: -12,
+
+      //МАСТЕР
+      masterVolume: -20,
+      masterReverbWet: 0.2,
+      masterBpm: 80
     }
   }
 
@@ -50,6 +55,77 @@ export default class Container extends Component {
     initToneNodes()
   }
 
+  handleMasterChange = (property, value) => {
+    this.setState({
+      [property]: value
+    })
+
+    // Применяем это изменение ко всем синтезаторам
+    if (property === 'masterVolume') {
+      setToneNodeProperty('bassVolume', value, 'bass')
+      setToneNodeProperty('melodyVolume', value, 'melody')
+      setToneNodeProperty('padVolume', value, 'pad')
+    }
+
+    if (property === 'masterReverbWet') {
+      setToneNodeProperty('bassReverbWet', value, 'bass')
+      setToneNodeProperty('melodyReverbWet', value, 'melody')
+      setToneNodeProperty('padReverbWet', value, 'pad')
+    }
+
+    if (property === 'masterBPM') {
+      setToneNodeProperty('bassBPM', value, 'bass')
+      setToneNodeProperty('melodyBPM', value, 'melody')
+      setToneNodeProperty('padBPM', value, 'pad')
+    }
+
+    sendToFirebase(property, value)
+  }
+
+  // handleChange = (property, value, type = '') => {
+  //   this.setState({
+  //     [property]: value
+  //   })
+
+  //   setToneNodeProperty(property, value, type)
+
+  //   if (property === 'bassSoundPreset') {
+  //     const bassPresetSettings = bassSettings?.presets?.[value]
+  //     if (bassPresetSettings) {
+  //       this.setState({
+  //         bassReverbWet: bassPresetSettings.reverbWet,
+  //         bassDistortionWet: bassPresetSettings.distortionWet,
+  //         bassVolume: bassPresetSettings.volume
+  //       })
+  //     } else {
+  //       console.warn(`Пресет ${value} не найден в bassSettings.presets`)
+  //     }
+  //   }
+
+  //   if (property === 'melodySoundPreset') {
+  //     const melodyPresetSettings = melodySettings?.presets?.[value]
+  //     if (melodyPresetSettings) {
+  //       this.setState({
+  //         melodyVolume: melodyPresetSettings.volume
+  //       })
+  //     } else {
+  //       console.warn(`Пресет ${value} не найден в melodySettings.presets`)
+  //     }
+  //   }
+
+  //   if (property === 'padSoundPreset') {
+  //     const padPresetSettings = padSettings?.presets?.[value]
+  //     if (padPresetSettings) {
+  //       this.setState({ padVolume: padPresetSettings.volume })
+
+  //       setToneNodeProperty('padVolume', padPresetSettings.volume, 'pad')
+  //     }
+  //   }
+
+  //   sendToFirebase(property, value)
+  // }
+
+  // Универсальная функция для обработки клика по кнопке
   handleChange = (property, value, type = '') => {
     this.setState({
       [property]: value
@@ -104,98 +180,132 @@ export default class Container extends Component {
   }
 
   renderUI = () => {
+    const { selectedSynth } = this.state
+
     return (
       <div className="Container">
-        <SC_ToggleButtonSet
-          name="Bass Sound Preset"
-          options={['default', 'preset1', 'preset2']}
-          value={this.state.bassSoundPreset}
-          property="bassSoundPreset"
-          handleChange={(property, value) =>
-            this.handleChange(property, value, 'bass')
-          }
-        />
+        {selectedSynth === 'bass' && (
+          <>
+            <SC_ToggleButtonSet
+              name="Bass Sound Preset"
+              options={['default', 'preset1', 'preset2']}
+              value={this.state.bassSoundPreset}
+              property="bassSoundPreset"
+              handleChange={(property, value) =>
+                this.handleChange(property, value, 'bass')
+              }
+            />
 
-        <SC_ToggleButtonSet
-          name="Melody Sound Preset"
-          options={['default', 'preset1', 'preset2']}
-          value={this.state.melodySoundPreset}
-          property="melodySoundPreset"
-          handleChange={(property, value) =>
-            this.handleChange(property, value, 'melody')
-          }
-        />
+            <SC_Slider
+              name="bass volume"
+              min={-60}
+              max={0}
+              step={5}
+              value={this.state.bassVolume}
+              property="bassVolume"
+              handleChange={this.handleChange}
+            />
+            <SC_Slider
+              name="Master Volume"
+              min={-60}
+              max={0}
+              step={1}
+              value={this.state.masterVolume}
+              property="masterVolume"
+              handleChange={this.handleMasterChange}
+            />
 
-        <SC_ToggleButtonSet
-          name="Pad Sound Preset"
-          options={['default', 'preset1', 'preset2']}
-          value={this.state.padSoundPreset}
-          property="padSoundPreset"
-          handleChange={(property, value) =>
-            this.handleChange(property, value, 'pad')
-          }
-        />
+            <SC_Knob
+              name="Master Reverb"
+              property="masterReverbWet"
+              min={0}
+              max={1}
+              step={0.1}
+              value={this.state.masterReverbWet}
+              handleChange={this.handleMasterChange}
+            />
 
+            <SC_Knob
+              name="bass reverb"
+              property="bassReverbWet"
+              min={0}
+              max={1}
+              step={0.1}
+              value={this.state.bassReverbWet || 0.3}
+              handleChange={this.handleChange}
+            />
+
+            <SC_Knob
+              name="bass distortion"
+              property="bassDistortionWet"
+              min={0}
+              max={1}
+              step={0.1}
+              value={this.state.bassDistortionWet || 0}
+              handleChange={this.handleChange}
+            />
+          </>
+        )}
+
+        {selectedSynth === 'melody' && (
+          <>
+            <SC_ToggleButtonSet
+              name="Melody Sound Preset"
+              options={['default', 'preset1', 'preset2']}
+              value={this.state.melodySoundPreset}
+              property="melodySoundPreset"
+              handleChange={(property, value) =>
+                this.handleChange(property, value, 'melody')
+              }
+            />
+
+            <SC_Slider
+              name="melody volume"
+              min={-60}
+              max={0}
+              step={5}
+              value={this.state.melodyVolume}
+              property="melodyVolume"
+              handleChange={this.handleChange}
+            />
+          </>
+        )}
+
+        {selectedSynth === 'pad' && (
+          <>
+            <SC_ToggleButtonSet
+              name="Pad Sound Preset"
+              options={['default', 'preset1', 'preset2']}
+              value={this.state.padSoundPreset}
+              property="padSoundPreset"
+              handleChange={(property, value) =>
+                this.handleChange(property, value, 'pad')
+              }
+            />
+
+            <SC_Slider
+              name="pad volume"
+              min={-60}
+              max={0}
+              step={5}
+              value={this.state.padVolume}
+              property="padVolume"
+              handleChange={this.handleChange}
+            />
+          </>
+        )}
+
+        {/* Можно добавить универсальные контролы вроде BPM, если нужно */}
         <SC_Slider
           name="BPM"
           min={0}
           max={300}
           step={1}
-          value={this.state.bpm}
-          property="bpm"
+          value={this.state.masterBpm}
+          property="masterBpm"
           handleChange={(property, value) =>
             this.handleChange(property, value, '')
           }
-        />
-
-        <SC_Slider
-          name="bass volume"
-          min={-60}
-          max={0}
-          step={5}
-          value={this.state.bassVolume}
-          property="bassVolume"
-          handleChange={this.handleChange}
-        />
-
-        <SC_Slider
-          name="melody volume"
-          min={-60}
-          max={0}
-          step={5}
-          value={this.state.melodyVolume}
-          property="melodyVolume"
-          handleChange={this.handleChange}
-        />
-
-        <SC_Slider
-          name="pad volume"
-          min={-60}
-          max={0}
-          step={5}
-          value={this.state.padVolume}
-          property="padVolume"
-          handleChange={this.handleChange}
-        />
-
-        <SC_Knob
-          name="bass reverb"
-          property="bassReverbWet"
-          min={0}
-          max={1}
-          step={0.1}
-          value={this.state.bassReverbWet || 0.3}
-          handleChange={this.handleChange}
-        />
-
-        <SC_Knob
-          name="bass distortion"
-          property="bassDistortionWet"
-          min={0}
-          max={1}
-          step={0.1}
-          value={this.state.bassDistortionWet || 0}
-          handleChange={this.handleChange}
         />
       </div>
     )
@@ -206,11 +316,24 @@ export default class Container extends Component {
   }
 
   render() {
-    const { isStarted } = this.state
+    const { isStarted, selectedSynth } = this.state
 
     return (
       <div className="Container">
-        {isStarted ? this.renderUI() : this.renderStartButton()}
+        {!isStarted ? (
+          <>
+            <SC_ToggleButtonSet
+              name="Выбор синтезатора"
+              options={['bass', 'melody', 'pad']}
+              value={selectedSynth}
+              property="selectedSynth"
+              handleChange={this.handleChange}
+            />
+            {this.renderStartButton()}
+          </>
+        ) : (
+          this.renderUI()
+        )}
       </div>
     )
   }
